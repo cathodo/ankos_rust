a3e1d0d8ace5642e88871f44df8546ba26f85a71
= <=>
2022-10-04 20:34:53 -0600

    resources and basic run window setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let gs = State{ };
+    main_loop(context, gs)
 }
