beafadcbc802623c1ddaf0954ed38c78ce351a46
= <=>
2022-10-06 14:56:40 -0600

    generation doesn't persist for some reason

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,158 @@
 use bracket_lib::prelude::*;
 use specs::prelude::*;
-mod components;
-pub use components::*;
-mod map;
-pub use map::*;
-mod automata_system;
-use automata_system::*;
 
-pub struct State {
-    pub ecs: World
+#[derive(PartialEq, Clone, Copy)]
+enum CellState {
+    Off = 0,
+    On = 1, 
 }
 
-impl State {
-    fn run_systems(&mut self) {
-        let mut ca = AutomataSystem{};
-        ca.run_now(&self.ecs);
+const RUNFREQUENCY: i32 = 6;
+const SCREENWIDTH: usize = 80;
+const SCREENHEIGHT: usize = 50;
+const SCREENCOUNT: usize = SCREENWIDTH*SCREENHEIGHT;
+// with layout [nw, n, ne, w, c, e, sw, s, se]
+const NEIGHBORHOOD: [(i32, i32); 9] = [
+    (-1,  1),
+    ( 0,  1),
+    ( 1,  1),
+    (-1,  0),
+    ( 0,  0),
+    ( 1,  0),
+    (-1, -1),
+    ( 0, -1),
+    ( 1, -1)
+];
+
+fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * SCREENWIDTH as usize) + x as usize
+}
+
+fn idx_xy(idx: usize) -> (i32, i32) {
+    let x = (idx % SCREENWIDTH) as i32;
+    let y = (idx as f32/SCREENWIDTH as f32).floor() as i32;
+    return (x, y)
+}
+
+fn moor_xy_2d(mut x: i32, mut y: i32) -> (i32, i32) {
+    let width = SCREENWIDTH as i32-1;
+    let height = SCREENHEIGHT as i32-1;
+
+    if x > width {
+        x -= width;
     }
+    if x < 0 {
+        x += width;
+    }
+    if y > height {
+        y -= height;
+    }
+    if y < 0 {
+        y += height;
+    }
+
+    (x, y)
+}
+
+fn spawn_initial_state() -> Vec<CellState> {
+    // all off
+    let mut state = vec![CellState::Off; SCREENCOUNT];
+
+    // add seed 
+    let idx = xy_idx(SCREENWIDTH as i32/2, SCREENHEIGHT as i32/2);
+    state[idx] = CellState::On;
+
+    state
+}
+
+fn ca_step(cells: Vec<CellState>, run: bool) -> Vec<CellState> {
+    let mut new_cells: Vec<CellState> = Vec::new();
+    if run {
+        let mut neighbour_count: i8 = 0;
+        let mut y = 0;
+        let mut x = 0;
+        for _idx in 0..SCREENCOUNT {
+            // count neighbours
+            for (moorx, moory) in NEIGHBORHOOD.iter() {
+                let (mx, my) = moor_xy_2d( x + moorx, y + moory );
+                let midx = xy_idx(mx, my);
+                if cells[midx] == CellState::On {
+                    neighbour_count += 1;
+                }
+                
+            }
+            // set cell type
+            //if neighbour_count < 2 || neighbour_count > 3 {
+            if neighbour_count < 1 {
+                new_cells.push(CellState::Off);
+            } else {
+                new_cells.push(CellState::On);
+            }
+    
+            // Move the coordinates
+            x += 1;
+            if x > SCREENWIDTH as i32-1 {
+                x = 0;
+                y += 1;
+            }
+        }
+    } else {
+        new_cells = cells;
+    }
+    new_cells
+}
+
+fn draw_cells(cells: &[CellState], ctx : &mut BTerm) {
+    let mut y = 0;
+    let mut x = 0;
+    for cell in cells.iter() {
+        // Render a tile depending upon the tile type
+        let fg;
+        let bg = RGB::from_f32(0., 0., 0.);
+        let glyph;
+        match cell {
+            CellState::Off => {
+                fg = RGB::named(GREY);
+                glyph = to_cp437('.');
+            }
+            CellState::On => {
+                fg = RGB::named(GREEN);
+                glyph = to_cp437('#');
+            }
+        }
+        ctx.set(x, y, fg, bg, glyph);
+
+        // Move the coordinates
+        x += 1;
+        if x > SCREENWIDTH as i32-1 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+struct State {
+    ecs: World
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx : &mut BTerm) {
         ctx.cls();
-
-        self.run_systems();
-
-        draw_map(&self.ecs, ctx);
         
-        let positions = self.ecs.read_storage::<Position>();
-        let renderables = self.ecs.read_storage::<Renderable>();
-        
-        for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        let cells = self.ecs.fetch::<Vec<CellState>>();
+        let mut compute = false;
+        // ca step
+        match ctx.key {
+            None => {}
+            Some(key) => match key {
+                VirtualKeyCode::Space => { compute = true; }
+                _ => {}
+            }
         }
+        // ca step
+        let new_cells = ca_step(cells.to_vec(), compute);
+
+        draw_cells(&new_cells, ctx);
     }
 }
 
@@ -43,11 +164,7 @@ fn main() -> BError {
     let mut gs = State {
         ecs: World::new()
     };
-    gs.ecs.register::<Position>();
-    gs.ecs.register::<Renderable>();
-
-    let map : Map = Map::new();
-    gs.ecs.insert(map);
+    gs.ecs.insert(spawn_initial_state());
 
     main_loop(context, gs)
 }
